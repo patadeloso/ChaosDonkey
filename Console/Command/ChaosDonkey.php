733f8cc3d20704cedc4ad73118fa553a2abf9c0c
Shaun McManus <shaun.mcmanus@gmail.com>
2023-06-29 22:58:57 -0700

    Add CLI basics

---
+++ Console/Command/ChaosDonkey.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+namespace ShaunMcManus\ChaosDonkey\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ChaosDonkey extends Command
+{
+
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setName('chaosdonkey:kick')
+            ->setDescription('A module that causes chaos randomly');
+
+        parent::configure();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Code goes here ...
+        return 0;
+    }
+}
+
+
